e1460cb8cf63456c793dbe3f008e849033400d2f
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2026-03-16 14:37:53 +0100

    updated

---
+++ AgentUgo.Dashboard/Program.cs
@@ -0,0 +1,39 @@
+using AgentUgo.Dashboard.Components;
+using Microsoft.AspNetCore.ResponseCompression;
+using Ugo.Orchestrator.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddSignalR();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        ["application/octet-stream"]);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseResponseCompression();
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.MapHub<AgentUgoHub>("/agentUgoHub");
+
+app.Run();
