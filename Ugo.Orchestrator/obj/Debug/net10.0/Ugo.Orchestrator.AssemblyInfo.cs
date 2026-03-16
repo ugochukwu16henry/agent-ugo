ac18ac8a308e31f957a8222b0deca3ade545cfe4
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2026-03-16 19:57:48 +0100

    updated

--- Ugo.Orchestrator/obj/Debug/net10.0/Ugo.Orchestrator.AssemblyInfo.cs
+++ Ugo.Orchestrator/obj/Debug/net10.0/Ugo.Orchestrator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ugo.Orchestrator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e57c74efa3c4657b4aa7de7dbea0eb145879db19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbf298ddeb81e2cb9236c1b1f52b16f6525e4cf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ugo.Orchestrator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ugo.Orchestrator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
